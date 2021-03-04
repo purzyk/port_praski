2d9f83624bc8da17bffd5014fc8ec9d1c34af5f0
Piotr Purzycki <ppurzycki@efresh.com.pl>
2021-03-04 21:51:51 +0100

    taxonomy inwestycje + kontakt

--- taxonomy-inwestycja.php
+++ taxonomy-inwestycja.php
@@ -5,8 +5,6 @@ $context          = Timber::get_context();
 $context['title'] = single_tag_title('', false);
 $context['post']        = $post;
 $term = get_queried_object();
-
-
 $glowne_zdjecie = get_field('glowne_zdjecie', $term);
 $slogan = get_field('slogan', $term);
 $lead = get_field('lead', $term);
@@ -19,5 +17,13 @@ $context['lead'] = $lead;
 $context['opis'] = $opis;
 $context['posts']      = new Timber\PostQuery();
 $context['pagination'] = Timber::get_pagination($pagination_mid_size);
-
+$cat = get_queried_object()->term_id;
+$lokale = Timber::get_posts(array('orderby' => 'title', 'post_type' => 'lokale','posts_per_page' => '-1', 'order' => 'ASC', 'tax_query' => array(
+    array(
+      'taxonomy' => 'inwestycja',
+      'terms'    => $cat,
+    ),
+  ),));
+$context['postsLoop'] = $lokale;
+$context['cat'] = $cat;
 Timber::render($templates, $context);
