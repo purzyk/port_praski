48d76000bfeece2d507ce2bd9d14d9891bcb0bd6
kaczorukpawel <pawel.kaczoruk@yslab.pl>
2021-03-08 09:33:37 +0100

    Fix slide autoplay when scrolling from bottom

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -582,16 +582,13 @@ function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
 
   return (
-    rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <=
-    (window.innerHeight ||
-      document.documentElement
-      .clientHeight) /* or $(window).height() */ &&
-    rect.right <=
-    (window.innerWidth ||
-      document.documentElement.clientWidth) /* or $(window).width() */
-  );
+    (
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ &&
+      rect.bottom >= 0
+    ) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+);
 }
 
 // TO REFACTOR
