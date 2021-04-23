e9b12c833aa6773b7292ce9d6640d80e43491c73
mateusz <mateusz.kawka@yslab.pl>
2021-04-23 14:33:48 +0200

    fix sliders marks

--- resources/assets/js/components/ApartmentsListFilters.vue
+++ resources/assets/js/components/ApartmentsListFilters.vue
@@ -42,7 +42,7 @@
                                 style="display: none"
                             />
                             <label :for="item.id"                  
-                                ><img class="filters-investment-image"
+                                ><img :src="item.image.sizes.thumbnail" class="filters-investment-image"
                             /></label>
                             <span class="checkboxTest__title">{{
                                 item.title
