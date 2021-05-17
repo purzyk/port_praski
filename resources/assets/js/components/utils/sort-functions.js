d272da7480e3b62c36bbc2756ccb7bb363d5f25f
mateusz <mateusz.kawka@yslab.pl>
2021-05-17 12:26:13 +0200

    fix sort by investment name

--- resources/assets/js/components/utils/sort-functions.js
+++ resources/assets/js/components/utils/sort-functions.js
@@ -1,11 +1,10 @@
-// do poprawki, coś się buguje ale nie pamiętam co
-
+// we need custom sort for investment name because apartment can have 1 or 2 investments
 export const sortByInvestmentName = (apartments, sortAsc = true) => {
     if (sortAsc) {
         return apartments.sort((a, b) => {
             let x = a.inwestycje.length > 1 ? a.inwestycje[1] : a.inwestycje[0];
             let y = b.inwestycje.length > 1 ? b.inwestycje[1] : b.inwestycje[0];
-            return x.inwestycje > y.inwestycje ? 1 : -1;
+            return x > y ? 1 : -1;
         });
     } else {
         return apartments.sort((a, b) => {
@@ -16,7 +15,7 @@ export const sortByInvestmentName = (apartments, sortAsc = true) => {
     }
 };
 
-
+// field is from wordpress apartment object eg. liczba_pokoi
 export const sortFunction = (apartments, sortAsc= true, field) => {
     if (sortAsc) {
         return apartments.sort((a, b) => {
