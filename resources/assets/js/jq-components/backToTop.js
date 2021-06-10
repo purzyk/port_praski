91ad676c57533d13ad2569c0dc0cb25be6016a5c
mateusz <mateusz.kawka@yslab.pl>
2021-06-10 10:18:58 +0200

    Fix scroll to top button

--- resources/assets/js/jq-components/backToTop.js
+++ resources/assets/js/jq-components/backToTop.js
@@ -1,36 +1,30 @@
 import $ from 'jquery'
 
-$(() => {
+
+window.addEventListener("load", () => {
   const toTop = document.querySelectorAll(".backToTop");
-  const YOffset = window.pageYOffset;
 
   [...toTop].forEach(item => {
     item.addEventListener("click", () => {
       $('body, html').animate({ scrollTop: 0 }, 800)
     })
   })
-  $(window).on('scroll', () => {
-  //   if ($(this).scrollTop() > 0) {
-  //     console.log("SCROLL TOP VISIBLE")
-  //     $('#ToTop').fadeIn()
-  //     return
-  //   }
-
-  //  // $('#ToTop').fadeOut()
-
-
+  document.addEventListener('scroll', ()=> {
+    let YOffset = window.pageYOffset
     if(YOffset > window.innerHeight / 2) {
       [...toTop].forEach(item => {
         item.classList.add("--show")
       })
     } else {
       [...toTop].forEach(item => {
-  
         item.classList.remove("--show")
       })
     }
-
   })
+})
+
+$(() => {
+  
 
 
   $('.header__logo').on('click', () => {
